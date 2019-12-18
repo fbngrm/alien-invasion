6d63f402e273059f03dfc667f2bd1c75c91a90ea
fbngrm <fbngrm@gmail.com>
2019-12-18 23:22:01 +0100

    enhance comments

--- world/map.go
+++ world/map.go
@@ -1,38 +1,36 @@
 package world
 
-// we use a directed graph to represent the world map.
-// since each city/vertex has a max vertex degree of 4, we assume to have a
-// sparse graph and thus use an adjacency list to reference neighboring vertices.
+// We use a directed graph to represent the world map. Since each city/vertex
+// has a max vertex degree of 4, we assume to have a sparse graph and thus,
+// use an adjacency list to reference neighboring cities/vertices.
 
-// adjacency list with cardinal directions indexed by neighboring city name
+// Adjacency list with cardinal directions
+// indexed by neighboring city name.
 type link map[string]string
 
-// World implements a generalized finite directed graph.
+// Map implements a generalized finite directed graph,
 // where cities act as nodes and roads as edges.
-// roads represent an adjacency list of a cities neighbors indexed by city ids.
-// cities indexed by name
 type Map map[string]link
 
-// Connect adds a road from city a to city b in direction d.
-// for convenience we store the inversed connection between all cities too.
-// thus we ensure that all connected cities get created.
+// Link adds a link from `origin` to `destination` in direction `direction`.
 func (m Map) Link(origin, destination, direction string) {
-	// outbound link a => b in direction d
+	// outbound link
 	if _, ok := m[origin]; !ok {
 		m[origin] = make(link)
 	}
 	m[origin][destination] = direction
 
-	// we need to initialize the linked destination since it may have inbound links only.
-	// if it has outbound links, it either exists already or will get overwritten
-	// in a subsequent call.
-	// inbound only cities still can get invaded and destroyed, so we need to pu them on the map.
+	// we need to initialize the linked destination since it may have inbound
+	// links only. if it has outbound links, it either exists already or will
+	// get overwritten in a subsequent call.
+	// inbound only cities still can get invaded and destroyed, so we need to
+	// put them on the map.
 	if _, ok := m[destination]; !ok {
 		m[destination] = make(link)
 	}
 }
 
-// destroyCity removes a city with given name from the world, as well as any
+// DestroyCity removes a city with given name from the map, as well as any
 // roads attached to it. no-op if a city with the given name does not exist.
 func (m Map) DestroyCity(cityName string) {
 	// destroy inbound links
@@ -43,6 +41,7 @@ func (m Map) DestroyCity(cityName string) {
 	delete(m, cityName)
 }
 
+// Cities returns a list of all cities on the map.
 func (m Map) Cities() []string {
 	cityNames := make([]string, len(m))
 	var i int
