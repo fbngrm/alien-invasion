74f248e8e85c14073f70345a48836738632f53df
fbngrm <fbngrm@gmail.com>
2019-12-18 21:36:23 +0100

    add invaded cities and tests

---
+++ invasion/cities.go
@@ -0,0 +1,36 @@
+package invasion
+
+import (
+	"math/rand"
+	"time"
+)
+
+// alien names by city name
+type invadedCities map[string]map[string]struct{}
+
+// distribute aliens pseudo randomnly
+func initInvadedCities(names alienNames, cityNames []string) invadedCities {
+	rand.Seed(time.Now().UTC().UnixNano())
+	invaded := make(invadedCities)
+	for _, alienName := range names {
+
+		// shuffle, potentially expensive for large slices
+		// todo: use random index or something more efficient
+		// and an interface to make it easier testable
+		for i := range cityNames {
+			j := rand.Intn(i + 1)
+			cityNames[i], cityNames[j] = cityNames[j], cityNames[i]
+		}
+
+		for _, cityName := range cityNames {
+			if len(invaded[cityName]) < MaxInvaders {
+				if _, ok := invaded[cityName]; !ok {
+					invaded[cityName] = make(map[string]struct{})
+				}
+				invaded[cityName][alienName] = struct{}{}
+				break
+			}
+		}
+	}
+	return invaded
+}
---
+++ invasion/cities_internal_test.go
@@ -0,0 +1,72 @@
+package invasion
+
+import (
+	"testing"
+)
+
+var invadedCitiesTests = []struct {
+	d string   // description of test case
+	a []string // aliens names
+	c []string // city names
+
+}{
+	{
+		d: "expect no aliens to be added",
+		a: []string{},
+		c: []string{"Foo", "Bar", "Baz", "Qu-ux"},
+	},
+	{
+		d: "expect one",
+		a: []string{"alien 1"},
+		c: []string{"Foo", "Bar", "Baz", "Qu-ux"},
+	},
+	{
+		d: "expect all aliens to be added",
+		a: []string{"alien 1", "alien 2", "alien 3"},
+		c: []string{"Foo", "Bar", "Baz", "Qu-ux"},
+	},
+	{
+		d: "expect all cites to be invaded by 2 aliens",
+		a: []string{"alien 1", "alien 2", "alien 3", "alien 4"},
+		c: []string{"Foo", "Bar"},
+	},
+}
+
+func TestInitInvadedCities(t *testing.T) {
+	for _, tt := range invadedCitiesTests {
+		a := make([]string, 0)
+		i := initInvadedCities(tt.a, tt.c)
+		for cityName, aliens := range i {
+			// consider MaxInvaders
+			if want, got := MaxInvaders, len(aliens); want < got {
+				t.Errorf("%s: max invader count %d but got %d", tt.d, want, got)
+			}
+			// all added cities are valid
+			if !contains(tt.c, cityName) {
+				t.Errorf("%s: unknown city %s", tt.d, cityName)
+			}
+			// all added aliens are valid
+			for alien := range aliens {
+				a = append(a, alien)
+				if !contains(tt.a, alien) {
+					t.Errorf("%s: unknown alien %s", tt.d, alien)
+				}
+			}
+		}
+		// all aliens have been added
+		for _, alien := range tt.a {
+			if !contains(a, alien) {
+				t.Errorf("%s: missing %s", tt.d, alien)
+			}
+		}
+	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
