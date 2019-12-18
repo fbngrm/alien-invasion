8a6d8971c2179808d2242deca443748303330e16
fbngrm <fbngrm@gmail.com>
2019-12-19 00:23:13 +0100

    fix typos

--- invasion/cities.go
+++ invasion/cities.go
@@ -8,7 +8,7 @@ import (
 // alien names by city name
 type invadedCities map[string]map[string]struct{}
 
-// distribute aliens pseudo randomnly
+// distribute aliens pseudo randomly.
 func initInvadedCities(names alienNames, cityNames []string) invadedCities {
 	rand.Seed(time.Now().UTC().UnixNano())
 	invaded := make(invadedCities)
@@ -16,7 +16,7 @@ func initInvadedCities(names alienNames, cityNames []string) invadedCities {
 
 		// shuffle, potentially expensive for large slices.
 		// todo: use random index or something more efficient
-		// and an interface to make test order deteministic.
+		// and an interface to make test order deterministic.
 		for i := range cityNames {
 			j := rand.Intn(i + 1)
 			cityNames[i], cityNames[j] = cityNames[j], cityNames[i]
